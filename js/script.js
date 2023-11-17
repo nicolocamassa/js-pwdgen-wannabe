1c159d77844b1e77168d10baa52606374fe795fc
nicolocamassa <camassanicolo03@gmail.com>
2023-11-17 14:45:33 +0100

    [ESERCIZIO FINITO] added password gen in .js file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let userName = prompt("Inserisci il tuo nome");
+let userSurname = prompt("Inserisci il tuo cognome");
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById('password-gen').innerHTML = userName + userSurname + favoriteColor + "23";
